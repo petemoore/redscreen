b5a119cfb699b97690ea34075481e732c91583df
Pete Moore <pmoore@mozilla.com>
2020-01-20 15:49:49 +0100

    wip

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -eu
 
 cd "$(dirname "${0}")"
-rm -rf dist
-mkdir dist
+rm -f dist/kernel8.img
+mkdir -p dist
 aarch64-unknown-linux-gnu-as -g -o homescreen.o homescreen.s 
 aarch64-unknown-linux-gnu-as -g -o font.o font.s 
 aarch64-unknown-linux-gnu-as -g -o uart.o uart.s 
@@ -12,7 +12,7 @@ aarch64-unknown-linux-gnu-ld -M -o homescreen.elf homescreen.o font.o uart.o del
 aarch64-unknown-linux-gnu-objcopy --set-start=0x80000 homescreen.elf -O binary dist/kernel8.img
 aarch64-unknown-linux-gnu-objdump -b binary -z --adjust-vma=0x80000 -maarch64 -D dist/kernel8.img
 rm homescreen.{o,elf} {mbox,uart,delays}.o
-# wget -q -O dist/LICENCE.broadcom https://github.com/raspberrypi/firmware/blob/d5b8d8d7cce3f3eecb24c20a55cc50a48e3d5f4e/boot/LICENCE.broadcom?raw=true
-# wget -q -O dist/bootcode.bin https://github.com/raspberrypi/firmware/blob/d5b8d8d7cce3f3eecb24c20a55cc50a48e3d5f4e/boot/bootcode.bin?raw=true
-# wget -q -O dist/fixup.dat https://github.com/raspberrypi/firmware/blob/d5b8d8d7cce3f3eecb24c20a55cc50a48e3d5f4e/boot/fixup.dat?raw=true
-# wget -q -O dist/start.elf https://github.com/raspberrypi/firmware/blob/d5b8d8d7cce3f3eecb24c20a55cc50a48e3d5f4e/boot/start.elf?raw=true
+[ -f dist/LICENCE.broadcom ] || wget -q -O dist/LICENCE.broadcom https://github.com/raspberrypi/firmware/blob/d5b8d8d7cce3f3eecb24c20a55cc50a48e3d5f4e/boot/LICENCE.broadcom?raw=true
+[ -f dist/bootcode.bin ] || wget -q -O dist/bootcode.bin https://github.com/raspberrypi/firmware/blob/d5b8d8d7cce3f3eecb24c20a55cc50a48e3d5f4e/boot/bootcode.bin?raw=true
+[ -f dist/fixup.dat ] || wget -q -O dist/fixup.dat https://github.com/raspberrypi/firmware/blob/d5b8d8d7cce3f3eecb24c20a55cc50a48e3d5f4e/boot/fixup.dat?raw=true
+[ -f dist/start.elf ] || wget -q -O dist/start.elf https://github.com/raspberrypi/firmware/blob/d5b8d8d7cce3f3eecb24c20a55cc50a48e3d5f4e/boot/start.elf?raw=true
